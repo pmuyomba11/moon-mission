dce064ac7069d30bdd27921f6162af927ef48c79
Peter Muyomba <ivanosorio@Ivans-MacBook-Pro.local>
2022-09-19 20:35:12 -0400

    index route updated

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 const express = require('express')
 const app = express()
+const marsMissions = require('./models/marsMissions.js')
 
 
-app.get('/', (req, res)=>{
-    res.send('working...')
+
+
+//Index route....
+app.get('/marsMissions', (req, res) => {
+    res.render('index.ejs', {
+      allMarsMissions: marsMissions  
+    })
+})
+
+//Show route
+app.get('/marsMissions/:id', (req,res)=>{
+    marsMission: marsMissions[req.params.id]
+    res.send(marsMissions[req.params.id])
 })
 
 
 //adding listener
-app.listen(4000, ()=>{
+app.listen(4000, () => {
     console.log("Server is running....");
 })
